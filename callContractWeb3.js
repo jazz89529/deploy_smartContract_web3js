3ade2b6e72fca57c435abc0d9a7f60651942f5ce
david <jazz89529@gmail.com>
2018-03-13 14:40:18 +0800

    add Comment

--- callContractWeb3.js
+++ callContractWeb3.js
@@ -4,9 +4,9 @@ const solc = require('solc');
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-let myContractAddress = '0xbe457c816bf0893cb3a152ce2d40d0c52bac2eb5';
+let myContractAddress = '0xbe457c816bf0893cb3a152ce2d40d0c52bac2eb5'; // decided by your contract deployed
 
-let abi = [
+let abi = [ // abi decided by your contract compiled
     {
         "constant": false,
         "inputs": [],
@@ -23,14 +23,12 @@ let abi = [
     }
 ]
 
-
 let myContract = web3.eth.contract(abi).at(myContractAddress);
 
-let data = myContract.renderHelloWorld.call();
-// let data = myContract.multiply.call(7);
+let data = myContract.renderHelloWorld.call(); // decided by your contract function
 
 console.log(data);
-// var contract = web3.eth.contract(myContractAddress, abi);
+
 
 
 
